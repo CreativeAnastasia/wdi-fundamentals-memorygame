17cf20efee0c03efadb4d54c9ef010adc97be5da
anastasiyav <anastasiyav@vsisoft.com>
2017-03-17 20:59:04 -0700

    created game logic

--- starter-code/main.js
+++ starter-code/main.js
@@ -26,38 +26,11 @@ function createBoard() {
 
     var cardElement = document.createElement('div');
     
-    cardElement.className = 'card';
-	
-    // setting the card to be an element of the array
-    cardElement.setAttribute('data-card', cards[i]);
-		// waiting for click event  so the function isTwoCards will run
-    cardElement.addEventListener('click', isTwoCards);
-
-    // apdd a card to the board
-    board.appendChild(cardElement);
-    board.appendChild(cardElement);
+   
   }
 
 }
-//function to check if the vards are in play
-function isTwoCards() {
-  // add card to array of cards being viewed
-  // 'this' is not covered in the pre-work but
-  // for now, just know it gives you access to the cardElement you click on
-  cardsInPlay.push(this.getAttribute('data-card'));
-	// card images
-	console.log(this.getAttribute('data-card'));
-	if (this.getAttribute('data-card') === 'king') {
-		this.innerHTML = "<img src='http://i.imgur.com/bnuv5Im.png'>"; // king image
-	} else {
-		this.innerHTML = "<img src='http://i.imgur.com/v6buNt2.png'>"; //queen image
-	}
-  // checking for match
-  if (cardsInPlay.length === 2) {
-    isMatch(cardsInPlay);
-    // clear array for next play
-    cardsInPlay = [];
-  }
+
 }
 
 function isMatch(cards) {
